7a83ece3a4c0e9e99bf31da7ebe658598d23ca52
Dlucasz027 <matheuslucas.027@gmail.com~>
2026-03-29 16:12:23 -0300

    TK-01: Add Course model with title, description, company, URL, creator (FK), and auto timestamp fields

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    company = models.CharField(max_length=50)
+    url = models.URLField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
